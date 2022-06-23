49389ce6e0f1459ed2f94d4a8b1254be5d3228f1
miwanotomoki <mmiwatomoki@gmail.com>
2022-06-23 14:59:56 +0900

    🩹 fix

--- lib/util/table_converter.py
+++ lib/util/table_converter.py
@@ -1,6 +1,5 @@
 from dataclasses import dataclass
 import pandas as pd
-import os
 
 
 @dataclass
@@ -25,24 +24,22 @@ class TableConverter():
 
     def __build_content_string(self, data: pd.core.frame.DataFrame) -> str:
         def __title_helper(cols: list[str]) -> str:
-            return "\\hline\n\t" + " & ".join(map(str, cols)) + " || \n\t\\hline \\hline"
+            return "\\hline\n\t" + " & ".join(map(str, cols)) + " \\\\ \n\t\\hline \\hline"
 
         def __content_helper(row: list[str]) -> str:
-            return "\t" + " & ".join(map(str, row)) + " || \n\t\\hline\n"
+            return "\t" + " & ".join(map(str, row)) + " \\\\ \n\t\\hline\n"
 
         col_num = len(data.columns)
         contents = ""
         for _, item in data.iterrows():
             contents += __content_helper(list(item))
-        return """
-    \\begin{{tabular}}{{{0}}}
+        return """\t\\begin{{tabular}}{{{0}}}
         {1}
         {2}
     \\end{{tabular}}""".format(col_num * 'c', __title_helper(data.columns), contents)
 
     def save_as_table(self, props: TableProps):
         content_string = self.__build_content_string(props.data)
-        print(os.curdir)
-        with open('../../paper/lib/src/table/{}'.format(props.file_name), mode='w') as f:
+        with open('../paper/lib/src/table/{}'.format(props.file_name + '.tex'), mode='w') as f:
             contents = self.__to_table_string(props.title, props.file_name, content_string)
             f.writelines(contents)
